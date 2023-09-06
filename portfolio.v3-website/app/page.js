cd73de238f9eb96e65e52c7d5067c434c501b967
John Ramos <jramos5883@gmail.com>
2023-09-06 15:19:06 -0700

    working on portfolio layout

--- portfolio.v3-website/app/page.js
+++ portfolio.v3-website/app/page.js
@@ -1,6 +1,7 @@
 export default function LandingPage() {
   return (
     <div>
+      {/* Need to plan layout for project section and mobile layout design */}
       <h1>Landing Page!</h1>
     </div>
   );
